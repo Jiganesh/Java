a39b2abcf81d1aecc6b56bafb82681e7a860573d
Jiganesh <jiganeshpatil01071999@gmail.com>
2021-06-23 04:35:04 +0530

    Wrote some codes from INFYTQ

---
+++ InfosysPlatform/Assignments/ConstructorAndThisKeywordAssignment1.java
@@ -0,0 +1,26 @@
+public class ConstructorAndThisKeywordAssignment1 {
+    public static void main(String[] args) {
+        OrderGiven O1 = new OrderGiven();
+        OrderGiven O2 = new OrderGiven(1001, "Garlic Shrimp");
+
+        System.out.println();
+        System.out.println(O1);
+        System.out.println(O2);
+    }
+
+}
+
+class OrderGiven{
+
+    public String status = "Ordered";
+
+    public OrderGiven(){
+        System.out.printf("Status of order 1 : %s\n", status);
+    }
+
+    public OrderGiven(int OrderId, String OrderFoods ){
+        System.out.printf("Status of order 2 : %s\n", status);
+        System.out.printf("Id of order 2 : %d\n", OrderId);
+        System.out.printf("Items ordered in order 2 : %s",OrderFoods);
+    }
+}
