77805ed7ee99404f9945333b7785c61287b12fb1
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2021-08-07 22:30:30 +0530

    learned about packages

---
+++ Problems/com/hello/hello.java
@@ -0,0 +1,10 @@
+package com.hello;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("This was Tutorial on how packages work");
+    }
+}
+
+// d:\GITHUB\Java\Problems\com\hello>javac -d . hello.java
+// d:\GITHUB\Java\Problems\com\hello>java com.hello.hello 
